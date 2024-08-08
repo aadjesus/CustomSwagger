0d7bc04ecf93f0012bed292424b3e4f83aef4215
aadjesus <aadjesus@outlook.com>
2024-08-08 15:46:32 -0300

    feat: Adicionando projeto

---
+++ CustomSwagger/wwwroot/swagger-ui/custom.js
@@ -0,0 +1,7 @@
+﻿var links = document.getElementsByTagName("link");
+for (var linkIndex = 0; linkIndex < links.length; linkIndex++) {
+	var link = links[linkIndex];
+	if (link && link.rel == 'icon') {
+		link.href = '/swagger-ui/favicon.ico';
+	}
+}
